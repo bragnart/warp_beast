91d993e2373f37472a99454f39619af34a325632
bragnart <tyontyen@gmail.com>
2025-12-10 01:12:04 +0700

    Refactor code structure for improved readability and maintainability

--- src/gui/components/dialog/base_message.py
+++ src/gui/components/dialog/base_message.py
@@ -10,6 +10,261 @@ COLORS = {
     
 }
 
+def make_md_style(
+        font: str, #шрифт для обычного текста, по умолчанию и к другим видам
+        text_size: int, #размер текста
+        main_text_color: str, #цвет основного обычного текста
+        h_text_color: str, #цвет заголовков
+        link_text_color: str, #цвет текста ссылок
+        img_text_color: str, #цвет текста заменяющего картинку
+        strong_text_color: str, #цвет жирного текста
+        em_text_color: str, #курсива
+        del_text_color: str, #зачеркнутого текста
+        cd_text_color: str, #inline блоков кода
+        bq_text_color: str, #цитат
+        cb_text_color: str, #чекбоксов
+        lb_text_color: str, # пунктов списков
+        th_text_color: str, #заголовков таблицы
+        tb_text_color: str, #пунктов таблицы
+        cd_block_color: str, #цвет inline блока кода
+        bq_block_color: str, #блока цитат
+        tc_block_color: str, #ячейки таблицы
+        line_color: str, # цвет линии (которая через ___ делается)
+        cd_font: str = None, #шрифт кода инлайн
+        bq_font: str = None, #шрифт цитат
+):
+    import copy
+    cd_font = cd_font or font
+    bq_font = bq_font or font
+    main_text_style = ft.TextStyle(
+        font_family=font,
+        size=text_size,
+        color=main_text_color
+    )
+    a_text_style = copy.deepcopy(main_text_style)
+    a_text_style.color = link_text_color
+    code_text_style = copy.deepcopy(main_text_style)
+    code_text_style.font_family = cd_font
+    code_text_style.color = cd_text_color
+    em_text_style = copy.deepcopy(main_text_style)
+    em_text_style.color = em_text_color
+    em_text_style.italic = True
+    strong_text_style = copy.deepcopy(main_text_style)
+    strong_text_style.color = strong_text_color
+    strong_text_style.weight = "bold"
+    del_text_style = copy.deepcopy(main_text_style)
+    del_text_style.color = del_text_color
+    del_text_style.decoration = "line_through"
+    bq_text_style = copy.deepcopy(main_text_style)
+    bq_text_style.color = bq_text_color
+    bq_text_style.font_family = bq_font
+    img_text_style = copy.deepcopy(main_text_style)
+    img_text_style.color = img_text_color
+    img_text_style.italic = True
+    cb_text_style = copy.deepcopy(main_text_style)
+    cb_text_style.color = cb_text_color
+    lb_text_style = copy.deepcopy(main_text_style)
+    lb_text_style.color = lb_text_color
+    th_text_style = copy.deepcopy(main_text_style)
+    th_text_style.color = th_text_color
+    th_text_style.weight = "bold"
+    tb_text_style = copy.deepcopy(main_text_style)
+    tb_text_style.color = tb_text_color
+    h = []
+    hsize = text_size
+
+    for _ in range(6):
+        s = copy.deepcopy(main_text_style)
+        s.color = h_text_color
+        s.size = hsize
+        h.append(s)
+        hsize += 3
+
+    
+
+    mds = ft.MarkdownStyleSheet(
+        a_text_style=a_text_style,
+        p_text_style=main_text_style,
+        code_text_style=code_text_style,
+        h1_text_style=h[5],
+        h2_text_style=h[4],
+        h3_text_style=h[3],
+        h4_text_style=h[2],
+        h5_text_style=h[1],
+        h6_text_style=h[0],
+        em_text_style=em_text_style,
+        strong_text_style=strong_text_style,
+        del_text_style=del_text_style,
+        blockquote_text_style=bq_text_style,
+        img_text_style=img_text_style,
+        checkbox_text_style=cb_text_style,
+        list_bullet_text_style=lb_text_style,
+        table_head_text_style=th_text_style,
+        table_body_text_style=tb_text_style,
+        codeblock_decoration=ft.BoxDecoration(
+            bgcolor=cd_block_color,
+            border=ft.border.all(0.5, ft.Colors.AMBER)
+        ),
+        blockquote_decoration=ft.BoxDecoration(
+            #bgcolor=bq_block_color,
+            border=ft.border.all(0.5, ft.Colors.PINK),
+            gradient=ft.LinearGradient(
+                begin=ft.alignment.top_left,
+                end=ft.alignment.bottom_right,
+                colors=[bq_block_color, ft.Colors.PINK_ACCENT]
+            )
+        ),
+        table_cells_decoration=ft.BoxDecoration(
+            bgcolor=ft.Colors.with_opacity(0.5, tc_block_color),
+            border=ft.border.all(0.5, tc_block_color)
+        ),
+        horizontal_rule_decoration=ft.BoxDecoration(
+            bgcolor=line_color
+        )
+    )
+    return mds
+
+dark_markdown_style = ft.MarkdownStyleSheet(
+
+
+    # Основные стили текста
+    p_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_100,
+        size=14,
+        font_family="gothra"
+    ),
+    
+    # Заголовки (h1-h6) - светлые и крупные
+    h1_text_style=ft.TextStyle(
+        color=ft.Colors.WHITE,
+        size=28,
+        weight="bold",
+        font_family="gothra"
+    ),
+    h2_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_50,
+        size=24,
+        weight="bold",
+        font_family="gothra"
+    ),
+    h3_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_100,
+        size=20,
+        weight="bold",
+        font_family="gothra"
+    ),
+    h4_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_200,
+        size=18,
+        weight="w500",
+        font_family="gothra"
+    ),
+    h5_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_200,
+        size=16,
+        weight="w500",
+        font_family="gothra"
+    ),
+    h6_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_200,
+        size=14,
+        font_family="gothra"
+    ),
+    
+    # Ссылки - яркие
+    a_text_style=ft.TextStyle(
+        color=ft.Colors.CYAN_400,
+        size=14,
+        font_family="gothra"
+    ),
+    
+    # Код в строке (inline)
+    code_text_style=ft.TextStyle(
+        color=ft.Colors.AMBER_200,
+        size=6,
+        font_family="gothra",
+        bgcolor=ft.Colors.GREY_900  # Тёмный фон для кода
+    ),
+    
+    # Блок кода
+    codeblock_padding=ft.padding.all(12),
+    codeblock_decoration=ft.BoxDecoration(
+        bgcolor=ft.Colors.RED_ACCENT,
+        border_radius=6
+    ),
+    
+    # Жирный текст
+    strong_text_style=ft.TextStyle(
+        color=ft.Colors.GREEN,
+        weight="bold",
+        font_family="gothra"
+    ),
+    
+    # Курсив
+    em_text_style=ft.TextStyle(
+        color=ft.Colors.DEEP_ORANGE_100,
+        italic=True,
+        font_family="gothra"
+    ),
+    
+    # Зачёркнутый текст
+    del_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_500,
+        decoration="line_through",
+        font_family="gothra"
+    ),
+    
+    # Кавычки
+    blockquote_text_style=ft.TextStyle(
+        color=ft.Colors.GREY_300,
+        size=14,
+        italic=True,
+        font_family="gothra"
+    ),
+    blockquote_padding=ft.padding.only(left=12, top=8, bottom=8, right=8),
+    blockquote_decoration=ft.BoxDecoration(
+        border=ft.border.only(
+            left=ft.BorderSide(2, ft.Colors.CYAN_400)
+        ),
+        bgcolor=ft.Colors.BLUE_GREY
+    ),
+    
+    # Пули списков
+    list_bullet_text_style=ft.TextStyle(
+        color=ft.Colors.CYAN_400,
+        font_family="gothra"
+    ),
+    
+    # Промежутки
+    block_spacing=16,
+    list_indent=24
+)
+
+
+md_style = make_md_style(
+    "sofia",
+    14,
+    "#082B1E",
+    "#042713",
+    "#05166E",
+    "#FFFFFF",
+    "#0E3124",
+    "#043926",
+    "#97AC0B",
+    "#C6641E",
+    "#931CB7",
+    "#043322",
+    "#606CE9",
+    "#004E31",
+    "#065739",
+    "#BC6069",
+    "#10BACD",
+    "#054B31",
+    "#ED00BD",
+    "manrope",
+    "arsenal"
+)
+
 class Avatar(ft.Column):
 
     def __init__(
@@ -18,6 +273,7 @@ class Avatar(ft.Column):
             image_path: str | Path,
             role_txt_color: str = "#BD5AA9",
             size: int = 50,
+            font: str = "Roboto",
             **kwargs
     ):
         super().__init__(**kwargs)
@@ -38,7 +294,8 @@ class Avatar(ft.Column):
             value=self.role.upper(),
             #text_align=ft.TextAlign.CENTER,
             color=self.role_txt_color,
-            weight=ft.FontWeight.BOLD
+            weight=ft.FontWeight.BOLD,
+            font_family=font,
         )
         self.controls = [
             self.pic,
@@ -65,7 +322,9 @@ class MsgTextColumn(ft.Column):
             value=self.msg_txt,
             selectable=True,
             extension_set=ft.MarkdownExtensionSet.COMMON_MARK,
-            code_theme=ft.MarkdownCodeTheme.ATELIER_DUNE_DARK,
+            code_theme=ft.MarkdownCodeTheme.BROWN_PAPER,
+            md_style_sheet=md_style,
+            
         )
         te = True if self.thought_txt is not None else False
         self.thought_txt_mkdn = ft.Markdown(
@@ -81,7 +340,7 @@ class MsgTextColumn(ft.Column):
             visible=te,
             on_click=self._th_click,
         )
-        self.scroll = ft.ScrollMode.AUTO
+        self.scroll = ft.ScrollMode.ALWAYS
         self.expand = True
         self.controls = [
             self.thought_txt_mkdn,
@@ -134,7 +393,7 @@ class MsgContainer(ft.Container):
             ],
 
         )
-        
+        self.bgcolor = ft.Colors.with_opacity(0.5, ft.Colors.ON_SECONDARY_CONTAINER)
 
 # class BaseMessageDialog(ft.Container):
 
@@ -258,18 +517,29 @@ if __name__ == '__main__':
 #     ```
 #     """
 #     THT = "Это мысли ИИ-агента, которые можно показать или скрыть по нажатию кнопки. **Это пример диалогового окна с аватаром, сообщением и кнопкой для отображения мыслей агента.**"*10
-    
+    src_dir = Path(__file__).parent.parent.parent.parent
+    asts_dir = src_dir / "assets"
+    fonts_dir = asts_dir / "fonts"
+    gr = fonts_dir / "gothra.ttf"
+    sf = fonts_dir / "sofia.ttf"
+    ar = fonts_dir / "arsenal.ttf"
+    mr = fonts_dir / "manrope.ttf"
+    dsp = asts_dir / "prompts" / "default_system_prompt.md"
+    dsp0 = dsp.read_text("utf-8")
     def main(page: ft.Page):
         page.window.icon = "icon.ico"
         page.fonts = {
-            "gothra": "https://github.com/bragnart/warp_beast/blob/dialog_windows/src/assets/fonts/gothra.ttf",
+            "gothra": str(gr),
+            "sofia": str(sf),
+            "arsenal": str(ar),
+            "manrope": str(mr),
         }
         page.theme_mode = ft.ThemeMode.DARK
         cnt = MsgContainer(
             width=500,
             height=200,
             avatar_size=50,
-            msg_txt="Ну что, поговорили?"
+            msg_txt=dsp0
         )
         page.add(cnt)
         
