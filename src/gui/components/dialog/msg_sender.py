a904c038bc93ddd45348b9a02d875e7f35d4be32
bragnart <tyontyen@gmail.com>
2025-12-10 10:29:52 +0700

    feat: refactor MsgSender component to use MsgSenderTheme for improved customization

--- src/gui/components/dialog/msg_sender.py
+++ src/gui/components/dialog/msg_sender.py
@@ -1,45 +1,65 @@
-# src/gui/components/msg_sender.py
+# src/gui/components/dialog/msg_sender.py
 import flet as ft
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class MsgSenderTheme(BaseModel):
+    width: int = Field(550)
+    height:int = Field(100)
+    input_font: str = Field("arsenal")
+    input_txtsize: int = Field(16)
+    input_bgcolor: str = Field("#7F85C4")
+    input_color: str = Field("#CD12BD")
+    input_bordercolor: str = Field("#ABF15A")
+    input_borderradius: int = Field(15)
+    btn_icon: str = Field("send")
 
 
-class MsgSender(ft.Container):
+class MsgSender(ft.Stack):
 
     def __init__(
             self,
-            width: int,
-            height: int,
-            txt_input_bgcolor: str,
-            txt_input_color: str,
-            txt_input_font: str,
-            txt_input_bordercolor: str,
+            theme: Optional[MsgSenderTheme] = None,
             **kwargs
     ):
         super().__init__(**kwargs)
-        self.width = width
-        self.height = height
+        if theme is None:
+            theme = MsgSenderTheme()
+        self.width = theme.width
+        self.height = theme.height
 
-        self.txt_input = ft.TextField(
-            keyboard_type=ft.KeyboardType.MULTILINE,
+        self.input = ft.TextField(
             multiline=True,
-            autofocus=True,
-            bgcolor=txt_input_bgcolor,
-            text_style=ft.TextStyle(
-                font_family=txt_input_font,
-                color=txt_input_color,
-            ),
-            border_radius=ft.border_radius.all(17),
-            border_color=txt_input_bordercolor,
             shift_enter=True,
+            autofocus=True,
+            expand=True,
+            text_style=ft.TextStyle(
+                size=theme.input_txtsize,
+                font_family=theme.input_font,
+                color=theme.input_color
+            ),
+            bgcolor=theme.input_bgcolor,
+            border_color=theme.input_bordercolor,
+            border_radius=ft.border_radius.all(theme.input_borderradius),
             on_submit=self._send,
         )
 
-
-        self.content = self.txt_input
+        self.send_btn = ft.IconButton(
+            icon=theme.btn_icon,
+            right=0,
+            top=0,
+            on_click=self._send,
+            opacity=0.5,
+        )
+        
+        self.controls.append(self.input)
+        self.controls.append(self.send_btn)
 
 
 
     async def _send(self, e):
-        msg_txt = self.txt_input.value
-        self.txt_input.value = ""
-        await self.page.data["send_msg_txt"](msg_txt)
+        msg_txt = self.input.value
+        self.input.value = ""
         self.update()
+        self.parent._send(msg_txt)
+
