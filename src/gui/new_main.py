047227e0a6c50372b946b9c3e5c1f7b35ada34a4
bragnart <tyontyen@gmail.com>
2025-12-10 13:37:56 +0700

    feat(gui): implement new main application with chat interface

---
+++ src/gui/new_main.py
@@ -0,0 +1,63 @@
+# src/gui/main.py
+from __future__ import annotations
+import flet as ft
+import asyncio
+
+from src.ai import AgentSession, AIConfig
+
+from .constants import ASSETS_PATH, FONT_DICT
+
+from .components.dialog import ChatView, ChatViewTheme, MsgContainer, MsgContainerTheme
+
+
+def main(page: ft.Page):
+    page.window.width = 560
+    page.window.height = 820
+    page.window.bgcolor = ft.Colors.TRANSPARENT
+    page.window.icon = "icon.ico"
+    page.window.center()
+    page.theme_mode = ft.ThemeMode.DARK
+    page.fonts = FONT_DICT
+
+
+    page.data = {
+        "themes": {},
+        "funcs": {},
+        "session": None,
+    }
+
+    ai_config = AIConfig(model_name="google/gemini-2.5-flash-lite", enable_mem0=False)
+
+    page.data["ai_config"] = ai_config
+
+    session = AgentSession(config=ai_config)
+    page.data["session"] = session
+
+    def on_keyboard_event(e: ft.KeyboardEvent):
+        if e.key == "Escape":
+            page.window.close()
+        if e.key == "Q" and e.ctrl:
+            page.window.close()
+
+
+    page.on_keyboard_event = on_keyboard_event
+
+    chat_view_theme = ChatViewTheme()
+
+    page.data["themes"]["chat_view"] = chat_view_theme
+
+    chat_view = ChatView(theme=page.data["themes"]["chat_view"])
+    
+    page.views.append(chat_view)
+
+    page.go("chat_view")
+
+    async def send_msg(msg_txt: str):
+        session: AgentSession = page.data["session"]
+        response = await session.run(user_msg=msg_txt)
+        chat_view._receive(response)
+    
+    page.data["funcs"]["send_msg"] = send_msg
+
+if __name__ == '__main__':
+    ft.app(target=main, assets_dir=str(ASSETS_PATH))
