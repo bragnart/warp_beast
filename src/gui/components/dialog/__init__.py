1b36d5648d700dbc615f6cf6c34a039a02010e03
bragnart <tyontyen@gmail.com>
2025-12-10 13:37:55 +0700

    feat(gui): add dialog components initialization

---
+++ src/gui/components/dialog/__init__.py
@@ -0,0 +1,21 @@
+# src/gui/components/dialog/__init__.py
+from .base_message import *
+from .chat_view import *
+
+__all__ = [
+    "MdTheme",
+    "create_markdown_style",
+    "random_codetheme",
+    "MsgTextTheme",
+    "MsgTextColumn",
+    "Avatar",
+    "AvatarTheme",
+    "ChatContainer",
+    "MsgContainer",
+    "ChatContainerTheme",
+    "MsgContainerTheme",
+    "MsgSender",
+    "MsgSenderTheme",
+    "ChatView",
+    "ChatViewTheme",
+]
