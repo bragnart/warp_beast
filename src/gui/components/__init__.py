4631f1f544817d2a17c69848e3aa9e4e1a60fddb
bragnart <tyontyen@gmail.com>
2025-12-11 16:03:29 +0700

    refactor(gui): clean up components module

--- src/gui/components/__init__.py
+++ src/gui/components/__init__.py
@@ -1,8 +1,1 @@
 # src/gui/components/__init__.py
-from .config_control import ConfigControl
-from .request_control import RequestControl
-
-__all__ = [
-    "ConfigControl",
-    "RequestControl",
-]
